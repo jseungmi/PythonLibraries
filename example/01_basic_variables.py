5271fd6a3fd794c09cf8ccfb203ca6c26be0daf6
sonjooyoung <reisei88.dev@gmail.com>
2019-09-24 22:04:27 +0900

    example files for session 2(1006)

---
+++ example/01_basic_variables.py
@@ -0,0 +1,21 @@
+#변수할당1
+a = "hello world"
+print(a)
+
+#변수할당2
+a1, a2 = "apple", "banana"
+print(a1, a2)
+
+#변수할당3
+b=c=d = 2
+print(b,c,d)
+
+#변수할당4
+mylist = [1, 2, 3, 4, 5]
+i1, i2, i3, i4, i5 = mylist
+print(i1,i2)
+
+#변수, 값, 바인딩1
+a = 100
+b = 100
+print(id(a), id(b))
---
+++ example/02_basic_datatype.py
@@ -0,0 +1,11 @@
+#Number - 정수형, 실수형
+a = 3
+print(type(a))
+b = 3.3
+print(type(b))
+a = float(a)
+print(type(a))
+c = 3.0
+print(type(c))
+
+#Number - 2진수, 8진수, 16진수
